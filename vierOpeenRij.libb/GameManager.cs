dba5a4c8a56660b50645719343fce04ac65e487b
Slicd <slicd.lichtherte@hotmail.com>
2016-12-31 01:49:29 +0100

    Added InsertClick method + better code

--- vierOpeenRij.libb/GameManager.cs
+++ vierOpeenRij.libb/GameManager.cs
@@ -3,55 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace vierOpeenRij.libb
 {
-    public class GameManager
+    public class GameManager // william -> todo : laat de spelers hun eigen naam kiezen.
     {
         
         Player Player1 = new Player
         {
-            Name = "Speler 1",
-                Color = 1 // geel
+            Name = "Speler 1 (Geel)",
+                Color = 1
         };
         Player Player2 = new Player
         {
-                Name = "Speler 2",
-                Color = 2 // rood
+                Name = "Speler 2 (Rood)",
+                Color = 2 
         };
 
+        enum color : int
+        {
+            empty = 0,
+            yellow = 1,
+            red = 2
+        }
+
+        int lastCoinColumn = 0,
+            lastCoinRow = 0;
+
         
+
+        public int CurrentColor = 1; // begint met speler 1
+
+               
 
         int[,] gameGrid = new int[7,7]; // de Multidimensional Array
         
        
-        public void ResetGameGrid()
+        public void CreateEmptyGameGrid()
         {
-            for(int column = 0; column < 7; column++)
+            for(int column = 1; column < 7; column++)
 			{
-			    for (int rij = 1; rij < 7; rij++)
+			    for (int rij = 0; rij < 7; rij++)
 			    {
-                    gameGrid[column, rij] = 0; // Alle hokjes leegmaken
+                    gameGrid[column, rij] = 0; // Alle hokjes leegmaken   
 			    }
 			}
         }
 
-        public int CurrentTurnChecker(int CurrentTurn)
+        public void NextTurn()
         {
-            if (CurrentTurn <= 1)
+            if (CurrentColor <= 1)
             {
-                return 2;
+               CurrentColor = 2;
             }
             else
             {
-                return 1;
+                CurrentColor = 1;
             }
         }
 
 
-        public string CurrentPlayer(int CurrentTurn) 
+        public string CurrentPlayer() //  William -> todo : font kleur overeen laten komen met het rondje.
         {
-            if(CurrentTurn <= 1)
+            if(CurrentColor <= 1)
             {
                 return Player1.Name;
             }
@@ -62,24 +77,44 @@ namespace vierOpeenRij.libb
             
         }
 
-        public void InsertCoin() // William
+        public void InsertCoin(int columnPara) // William -> variabele bijhouden met positie van laatst ingeworpen muntje
+        {
+            for(int i = 6; i > 1; i--)
+            {
+                if (gameGrid[columnPara, i]==0)
+                {
+                    gameGrid[columnPara, i] = CurrentColor;     //voegt het rondje toe 
+                    
+                    return;
+                }
+            }
+
+             
+        }
+
+       
+        public void VerticalCheck(/*positie van laatst ingeworpen muntje meegeven*/) //WILLIAM
+        {
+            //voorgestelde aanpak (probeer gerust iets anders als je iets anders bedenkt)
+
+                //onthou de laatste column waar is geInsert.
+                //gebruik een for loop om door de kolom het kleur te checken
+                //als het de waarde niet nul is, hou het aantal opeenvolgende kleur bij
+                //als het kleur verandert, reset de counter
+                //if counter = 4, victory!
+        }
+
+        public void HorizontalCheck(/*zie hierboven*/) // WILLIAM
         {
 
         }
 
-        public void HorizontalCheck()
+
+        public void DiagonalCheck(/*zie hierboven*/) 
         {
 
         }
 
-        public void VerticalCheck()
-        {
-
-        }
-
-        public void DiagonalCheck()
-        {
-
-        }
+       
     }
 }
